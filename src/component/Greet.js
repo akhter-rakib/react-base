f868c4c3f433909048d975a1a0e5f52124e277ac
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-02-18 09:59:36 +0600

    props destructuring in the method body.

--- src/component/Greet.js
+++ src/component/Greet.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Greet = ({ fname, lname }) => {//if you have multiple line then use {}
+const Greet = (props) => {//if you have multiple line then use {}
+    const {fname, lname} = props;
     return (// if you have multiple line in return statement then use ()
         <div>
             <h1> Hello your first Name is = {fname}! and last name {lname} </h1>
